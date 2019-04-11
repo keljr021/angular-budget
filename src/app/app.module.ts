4bed51c1ec9149ff379ccc46d67fb677c7a69fce
klmorr22 <keljr021@gmail.com>
2019-04-11 18:16:06 -0400

    Removed the step components from the build page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './layout/home/home.component';
 import { BuildTableComponent } from './layout/build-table/build-table.component';
-import { StructureComponent } from './components/structure/structure.component';
 import { OutputTableComponent } from './components/output-table/output-table.component';
-import { ValuesComponent } from './components/values/values.component';
-import { StylesComponent } from './components/styles/styles.component';
-import { DownloadComponent } from './components/download/download.component';
 import { HeadComponent } from './layout/head/head.component';
 import { FootComponent } from './layout/foot/foot.component';
 
@@ -21,11 +17,7 @@ import { MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule, Mat
     AppComponent,
     HomeComponent,
     BuildTableComponent,
-    StructureComponent,
     OutputTableComponent,
-    ValuesComponent,
-    StylesComponent,
-    DownloadComponent,
     HeadComponent,
     FootComponent
   ],
